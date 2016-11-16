e9f3db1d567cecf7a9cb11c23dd145c97d38c4e5
Julio Cesar Goldschmidt <julio.gold@gmail.com>
2016-11-16 20:51:54 -0200

    Fix AdressesController

--- src/WebApiSample/Controllers/Api/AdressesController.cs
+++ src/WebApiSample/Controllers/Api/AdressesController.cs
@@ -3,55 +3,52 @@ using System.Web.Http;
 
 namespace WebApiSample.Controllers.Api
 {
-    public class AddressController : ApiController
+    [RoutePrefix("Api/Adresses")]
+    public class AdressesController : ApiController
     {
-        [RoutePrefix("Api/Adresses")]
-        public class AdressesController : ApiController
+        [HttpGet]
+        [Route("Address")]
+        public IList<Address> Adresses()
         {
-            [HttpGet]
-            [Route("Address")]
-            public IList<Address> Adresses()
-            {
-                return new List<Address>() { new Address() };
-            }
-
-            [HttpGet]
-            [Route("Address")]
-            public Address Address(int id)
-            {
-                return new Address();
-            }
-
-            [HttpPost]
-            [Route("Address")]
-            public void CreateAddress([FromBody]Address model)
-            {
-            }
-
-            [HttpPut]
-            [Route("Address")]
-            public void EditAddress([FromBody]Address model)
-            {
-            }
-
-            [HttpDelete]
-            [Route("Address")]
-            public void DeleteAddress(int id)
-            {
-            }
+            return new List<Address>() { new Address() };
         }
 
-        public class Address
+        [HttpGet]
+        [Route("Address")]
+        public Address Address(int id)
         {
-            public int Id { get; set; }
-
-            public AddressType Type { get; set; }
+            return new Address();
         }
 
-        public enum AddressType
+        [HttpPost]
+        [Route("Address")]
+        public void CreateAddress([FromBody]Address model)
         {
-            Residential = 1,
-            Commercial = 2
         }
+
+        [HttpPut]
+        [Route("Address")]
+        public void EditAddress([FromBody]Address model)
+        {
+        }
+
+        [HttpDelete]
+        [Route("Address")]
+        public void DeleteAddress(int id)
+        {
+        }
+    }
+
+    public class Address
+    {
+        public int Id { get; set; }
+
+        public AddressType Type { get; set; }
+    }
+
+    public enum AddressType
+    {
+        Residential = 1,
+        Commercial = 2
     }
 }
